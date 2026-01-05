682c94e6b7cf1632648bff52a3680dbdd201aa3b
Daniel Dantas <contatodanieldantasdev@gmail.com>
2026-01-04 22:16:56 -0300

    feat: adição do arquivo project-create-edit.js

---
+++ developments/Co╠üdigo-Fonte-14/scripts/project-create-edit.js
@@ -0,0 +1,43 @@
+window.onload = function() {
+    // Type: 'create' | 'edit'
+    const screenType = 'create';
+
+    // MODO CRIAR
+    if(screenType == 'create') {
+        document.querySelector('#main-title').innerText = "Vamos cadastrar seu novo projeto!";
+        document.querySelector('#action-button').innerText = "Cadastrar";
+    }
+
+    // Fazer o Metodo de cadastrar
+    // Construir a massa dados do payload
+    // Enviar os dados para a API e redirecionar para a listagem
+
+    // MODO EDITAR
+    // if(screenType == 'edit') {
+    //     document.querySelector('#main-title').innerText = "Editar projeto";
+    //     document.querySelector('#action-button').innerText = "Salvar";
+    // }
+}
+
+function cadastrar() {
+    // Inicia a massa de dados (payload)
+    let payload = {
+        title: document.querySelector("#title").value,
+        totalCost: document.querySelector("#totalCost").value,
+        description: document.querySelector("#description").value,
+        idClient: "1"
+    }
+
+    // Enviar para API
+    fetch("https://622cd1e6087e0e041e147214.mockapi.io/api/projects", {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(response => {
+        alert('Cadastrado com sucesso!');
+    })
+}
